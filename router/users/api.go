38a2a1a3ac6c5c5417b41724c3bf33a6282b7001
krehwell <fahrimuham001@gmail.com>
2022-10-30 01:21:28 +0700

    lets start clean

--- router/users/api.go
+++
@@ -1,8 +0,0 @@
-package users
-
-import "github.com/gin-gonic/gin"
-
-func InitializeUserRoutes(r *gin.RouterGroup) {
-	r.POST("/users/register", registerUser)
-	r.GET("/users/login", loginUser)
-}
--- util/util.go
+++
@@ -1,7 +0,0 @@
-package util
-
-type ErrMsg struct {
-	ErrorMessage string
-}
-
-var JWT_KEY = "MY_SUPER_JWT_SECRET_KEY"
